5224e131ae4a60a25221043cf21419649fcb6500
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-09-03 01:24:30 -0300

    create nav component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, FooterComponent],
+	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
