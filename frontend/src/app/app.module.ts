42f5e0c00aa53a380d69c1dd772f59a2a50296c5
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-09-04 18:53:05 -0300

    create navigation between views

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,9 +13,10 @@ import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
+	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
