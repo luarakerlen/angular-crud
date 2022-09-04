dc8962400351ff74ccec81a880c79ab0cd8a2b27
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-09-04 18:43:37 -0300

    create home view

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
@@ -21,6 +23,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 		MatToolbarModule,
 		MatSidenavModule,
 		MatListModule,
+		MatCardModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
