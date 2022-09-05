7ea8347ff7c249efacd81260d4e73c9b1322ed84
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-09-04 23:19:23 -0300

    create read products component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
 
 @NgModule({
 	declarations: [
@@ -33,6 +34,7 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 		ProductCrudComponent,
 		RedDirective,
 		ProductCreateComponent,
+  ProductReadComponent,
 	],
 	imports: [
 		BrowserModule,
