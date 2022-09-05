7a917dbba5d0bb3f1b2937455fe7a467647a9ce9
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-09-05 17:33:22 -0300

    implement title change

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../../components/template/header/header.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+	constructor(private headerService: HeaderService) {
+		headerService.headerData = {
+			title: 'Início',
+			icon: 'home',
+			routerUrl: '',
+		};
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
